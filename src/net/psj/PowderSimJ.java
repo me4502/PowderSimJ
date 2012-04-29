166e71bc50a60e36723338e3b2e441960d40e1a1
Madeline Miller <mnmiller1@me.com>
2012-04-29 19:41:30 +1000

    Added

--- src/net/psj/PowderSimJ.java
+++ src/net/psj/PowderSimJ.java
@@ -1,5 +1,38 @@
 package net.psj;
 
-public class PowderSimJ {
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
 
+public class PowderSimJ extends BasicGame{
+	
+	public PowderSimJ()
+    {
+        super("PowderSimJ");
+    }
+ 
+    @Override
+    public void init(GameContainer gc) throws SlickException {
+ 
+    }
+ 
+    public static void main(String[] args) 
+			throws SlickException
+    {
+         AppGameContainer app = new AppGameContainer(new PowderSimJ());
+         app.setDisplayMode(800, 600, false);
+         app.start();
+    }
+
+	@Override
+	public void render(GameContainer arg0, Graphics arg1) throws SlickException {
+		
+	}
+
+	@Override
+	public void update(GameContainer arg0, int arg1) throws SlickException {
+		
+	}
 }

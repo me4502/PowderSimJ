6a952babf4f2653134377951e6c0106d057bc06b
Madeline Miller <mnmiller1@me.com>
2012-04-29 20:27:35 +1000

    Air Simulation

--- src/net/psj/PowderSimJ.java
+++ src/net/psj/PowderSimJ.java
@@ -1,12 +1,21 @@
 package net.psj;
 
+import net.psj.Simulation.Air;
+
 import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.BasicGame;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 
 public class PowderSimJ extends BasicGame{
+	
+	public static final int width = 800;
+	public static final int height = 600;
+	public static final int cell = 4;
+	
+	public Air air = new Air();
 	
 	public PowderSimJ()
     {
@@ -18,21 +27,27 @@ public class PowderSimJ extends BasicGame{
  
     }
  
-    public static void main(String[] args) 
-			throws SlickException
+    public static void main(String[] args) throws SlickException
     {
          AppGameContainer app = new AppGameContainer(new PowderSimJ());
-         app.setDisplayMode(800, 600, false);
+         app.setDisplayMode(width, height, false);
          app.start();
     }
 
 	@Override
 	public void render(GameContainer arg0, Graphics arg1) throws SlickException {
-		
+		air.drawAir();
 	}
 
 	@Override
 	public void update(GameContainer arg0, int arg1) throws SlickException {
+		Input input = arg0.getInput();
+		onKeypress(input);
+		air.update_air();
+	}
+	
+	public void onKeypress(Input keys)
+	{
 		
 	}
 }

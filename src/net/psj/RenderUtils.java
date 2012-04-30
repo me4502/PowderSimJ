d022a72ca944a14e32e430bea4d3eb86308fcbab
me4502 <mnmiller1@hotmail.com>
2012-04-30 16:09:22 +1000

    More Modes

--- src/net/psj/RenderUtils.java
+++ src/net/psj/RenderUtils.java
@@ -12,4 +12,18 @@ public class RenderUtils {
 		for(int w = width; w > 0; w--)
 			g.drawRect(x, y, w, height);
 	}
+	
+	public static int clamp_flt(float f, float min, float max)
+	{
+		if (f<min)
+			return 0;
+		if (f>max)
+			return 255;
+		return (int)(255.0f*(f-min)/(max-min));
+	}
+	
+	public static int PIXRGB(int r, int g, int b) 
+	{
+		return ((((r)<<8)&0xF800)|(((g)<<3)&0x07E0)|(((b)>>3)&0x001F));
+	}
 }

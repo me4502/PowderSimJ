4f97dbae5debe6015347589d8abfad106568c591
Madeline Miller <mnmiller1@me.com>
2012-05-02 11:39:22 +1000

    Performance Increase

---
+++ src/Interface/Menu.java
@@ -0,0 +1,14 @@
+package Interface;
+
+import net.psj.PowderSimJ;
+import net.psj.RenderUtils;
+
+public class Menu {
+
+	public static void draw()
+	{
+		int w = PowderSimJ.width;
+		int h = PowderSimJ.height;
+		RenderUtils.drawRect(0, h-10, w, h, 0xFFFFFF);
+	}
+}

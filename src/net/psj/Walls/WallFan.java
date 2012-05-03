1ffdb0e539d66907538dd7de387c51f6881af8aa
me4502 <mnmiller1@hotmail.com>
2012-05-03 16:41:31 +1000

    Better Stuff

---
+++ src/net/psj/Walls/WallFan.java
@@ -0,0 +1,12 @@
+package net.psj.Walls;
+
+import net.psj.RenderUtils;
+import net.psj.Simulation.Wall;
+
+public class WallFan extends Wall{
+
+	public void render(int x, int y)
+	{
+		RenderUtils.drawRect(x*CELL, y*CELL, (x*CELL)+CELL, (y*CELL)+CELL, 0,0,255);
+	}	
+}

73a955a61d994911f61f5dd591176d6ce29f14f4
me4502 <mnmiller1@hotmail.com>
2012-06-17 09:42:53 +1000

    Fixed a few bugs + added fancy fire graphics.

--- src/net/psj/Simulation/ShaderData.java
+++ src/net/psj/Simulation/ShaderData.java
@@ -1,18 +1,20 @@
 package net.psj.Simulation;
 
+import net.psj.ResourceLoader;
 import net.psj.shader.Shader;
 
 public class ShaderData {
 
-	public static Shader blurH, blurV, fancy, FXAA;
+	public static Shader blurH, blurV, fancy, FXAA, fire;
 
 	public static void init() {
 		try {
-			blurH = new Shader("res/simplevertex.vert",
-					"res/blurHorizontal.frg");
-			blurV = new Shader("res/simplevertex.vert", "res/blurVertical.frg");
-			fancy = new Shader("res/shock.vert", "res/shock.frag");
-			FXAA = new Shader("res/FXAA.vert", "res/FXAA.frag");
+			blurH = new Shader(ResourceLoader.loadResource("simplevertex.vert"),
+					ResourceLoader.loadResource("blurHorizontal.frg"));
+			blurV = new Shader(ResourceLoader.loadResource("simplevertex.vert"), ResourceLoader.loadResource("blurVertical.frg"));
+			fancy = new Shader(ResourceLoader.loadResource("shock.vert"), ResourceLoader.loadResource("shock.frag"));
+			FXAA = new Shader(ResourceLoader.loadResource("FXAA.vert"), ResourceLoader.loadResource("FXAA.frag"));
+			fire = new Shader(ResourceLoader.loadResource("fireVertex.vert"), ResourceLoader.loadResource("fireFrag.frg"));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}

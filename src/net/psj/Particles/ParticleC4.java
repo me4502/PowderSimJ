eeb1c18e0379574318ab8eed3b7f162c0add1124
me4502 <mnmiller1@hotmail.com>
2012-06-17 10:02:40 +1000

    Explosives.

---
+++ src/net/psj/Particles/ParticleC4.java
@@ -0,0 +1,12 @@
+package net.psj.Particles;
+
+import net.psj.Interface.MenuData;
+
+public class ParticleC4 extends Particle{
+
+	public ParticleC4() {
+		super("C4", new float[] { 1.0f, 0.81f, 0.06f }, 0.0f, 0.90f, 0.0f,
+				0.0f, 0.0f, 0.0f, 1000, 1000, 0, MenuData.MENU_PARTS);
+	}
+	
+}
